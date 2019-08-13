f50ceed8da3c82ecd528b499eceec20710babc5a
cjw <843562715@qq.com>
2019-08-13 11:29:10 +0800

    8.13 加入了网页嵌套

---
+++ PropertyManagementRun/src/main/resources/static/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * 系统主管理JS
+ */
+
+$(function(){
+	//点击左面功能菜单处理
+	$("ul#side-menu li ul li a").on("click",function(event){
+		var url=$(this).attr("href");
+		$("section#maincontent").load(url); //载入对应的对象管理主页
+		event.preventDefault();  //停止对象的默认行为。
+	});
+	
+});
